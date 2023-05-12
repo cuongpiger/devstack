08bf7d224a7a8d12946281ce95b518ade0e8080f
cuongpiger <cuongpigerr@gmail.com>
2023-05-12 17:14:31 +0700

    This is Manh Cuong's commit 🌻.

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Name of the conda environment to find
-if [ ! "$(docker ps -aqf "name=openstack-rabbitmq" 2>/dev/null)" ]; then
-    echo "Container exists"
-else
-    echo "Hello"
-fi
